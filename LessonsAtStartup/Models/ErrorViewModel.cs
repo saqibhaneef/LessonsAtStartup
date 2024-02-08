89773204b7411def14e6e4bb56dc42552aebb465
saqibhaneef <muhammadsaqibhanif80@gmail.com>
2024-02-08 18:53:49 +0500

    Add project files.

---
+++ LessonsAtStartup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LessonsAtStartup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
