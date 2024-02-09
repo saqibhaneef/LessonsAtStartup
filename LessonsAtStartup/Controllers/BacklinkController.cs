c7d65752f3b1355321c248c51a8b8e80189ab8d1
saqibhaneef <muhammadsaqibhanif80@gmail.com>
2024-02-09 18:34:09 +0500

    Backlink and Category controller added

---
+++ LessonsAtStartup/Controllers/BacklinkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LessonsAtStartup.Controllers
+{
+    public class BacklinkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
